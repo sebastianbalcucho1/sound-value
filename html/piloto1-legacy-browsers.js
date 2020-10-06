e43b5f0fb83b3d4ea55229519782676166c7b16b
Sebastián Balcucho <sebastianbalcucho@gmail.com>
2020-10-06 16:28:44 -0500

    _

--- html/piloto1-legacy-browsers.js
+++ html/piloto1-legacy-browsers.js
@@ -452,8 +452,6 @@ function graveRoutineEnd(snapshot) {
     Sonido.stop();  // ensure sound has stopped at end of routine
     psychoJS.experiment.addData('slider_2.response', slider_2.getRating());
     psychoJS.experiment.addData('slider_2.rt', slider_2.getRT());
-    nuestro_slider.push(slider_2.getRating());
-    
     return Scheduler.Event.NEXT;
   };
 }
