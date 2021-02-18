968ff8229c4e04815fb966ec80ae67ead0be028a
Sebastián Balcucho <sebastianbalcucho@gmail.com>
2021-02-17 19:15:20 -0500

    17_02_21

--- html/piloto2.js
+++ html/piloto2.js
@@ -28,7 +28,7 @@ psychoJS.openWindow({
 
 // store info about the experiment session:
 let expName = 'piloto2';  // from the Builder filename that created this script
-let expInfo = {'cedula': 'INGRESE SU CEDULA AQUÍ'};
+let expInfo = {'participant': 'INGRESE SU CEDULA AQUÍ'};
 
 // schedule the experiment:
 psychoJS.schedule(psychoJS.gui.DlgFromDict({
