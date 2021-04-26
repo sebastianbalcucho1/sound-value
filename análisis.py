3e9240624a0793004921b468e574c6a3f7256b79
Daniel Duque Lozano <dduque80@hotmail.com>
2021-04-26 00:06:21 +0000

    Replace análisis.py

--- análisis.py
+++ análisis.py
@@ -3,6 +3,9 @@ import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as stats
+import lux
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
 
 vinc=r"C:\Users\usuario\Google Drive\universidad\semillero\ruido experimento\data"
 total=pd.DataFrame()
@@ -19,20 +22,33 @@ for i in os.listdir(vinc):
         total=total.append(df2)
 
 def daniel():
-    print(total.groupby("Hertz").mean()["slider_exp.response"])
-    print(total.groupby("Hertz").mean()["slider_exp.rt"])
-    ax = sns.boxplot(x='Hertz', y="slider_exp.response", data=total, color='#99c2a2')
-    ax = sns.swarmplot(x='Hertz', y="slider_exp.response", data=total, color='#7d0013')
+    print("hola")
+    total1=total
+    total1["Hertz3"]=total.apply(lambda row: row["Hertz"] if row["volumen"]!=0 else 0,axis=1)
+    print(total1.groupby("Hertz3").mean()["slider_exp.response"])
+    print(total1.groupby("Hertz3").mean()["slider_exp.rt"])
+    ax = sns.boxplot(x='Hertz3', y="slider_exp.response", data=total1, color='#99c2a2')
+    ##ax = sns.swarmplot(x='Hertz', y="slider_exp.response", data=total, color='#7d0013')
     plt.show()
 
+    total4=total1.groupby(["Hertz3", "Random"]).mean()["slider_exp.response"].reset_index()
+    total1["resp"]=total1["slider_exp.response"]
+    total1["hertz"]=total1["Hertz3"].apply(lambda x: str(x))
+    model = ols('resp ~ hertz', data=total1).fit()
+    anova_table = sm.stats.anova_lm(model, typ=2)
+    print(anova_table)
 
-def sebastian():
 
+def sebastianB():
+    ...
 
 def ana():
+    print("hola")
 
-
+def sebastianS():
+    ...
 
 daniel()
-sebastian()
+sebastianS()
+sebastianB()
 ana()
